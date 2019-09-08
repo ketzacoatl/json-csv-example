ad65c991c2661b8bb7d798894c7e4cffe599ec7c
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2019-09-08 07:24:38 -0400

    WIP read JSON write CSV

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,106 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Lib
     ( someFunc
     ) where
 
-import           Data.Aeson
-import qualified Data.ByteString.Lazy as B
 
+import           Data.Aeson (eitherDecode, FromJSON, ToJSON)
+import           Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy as BS
+import           Data.Csv
+  ( encodeDefaultOrderedByName
+  , DefaultOrdered(headerOrder)
+  , Header
+  , namedRecord
+  , ToNamedRecord(toNamedRecord)
+  , (.=)
+  )
+
+import qualified Data.Csv as CSV
+import qualified Data.Foldable as Foldable
+import           Data.Vector (Vector)
+import qualified Data.Vector as Vector
+import           Data.Text (Text)
+import           GHC.Generics (Generic)
+import           Control.Monad.IO.Class (liftIO)
+
+
+csvHeader :: CSV.Header
+csvHeader =
+  Vector.fromList
+    [ "Qux"
+    , "Bar"
+    , "Foobar"
+    ]
+
+data JsonItem = JsonItem
+  { foo :: Int
+  , bar :: Text
+  } deriving (Show, Generic)
+
+data JsonItems = JsonItems [JsonItem]
+  deriving (Show, Generic)
+
+data CsvItem = CsvItem
+  { cItemQux :: Text
+  , cItemBar :: Text
+  , cItemFoobar :: Int
+  } deriving (Show, Generic)
+
+-- use generics
+instance FromJSON JsonItem
+instance ToJSON   JsonItem
+
+instance FromJSON JsonItems
+instance ToJSON   JsonItems
+
+instance CSV.DefaultOrdered CsvItem where
+  headerOrder _ =
+    CSV.header
+      [ "Qux" -- .= cItemQux
+      , "Bar" -- .= cItemBar
+      , "Foobar" -- .= cItemFoobar
+      ]
+
+instance CSV.ToNamedRecord CsvItem where
+  toNamedRecord CsvItem{..} =
+    CSV.namedRecord
+      [ "Qux" .= cItemQux
+      , "Bar" .= cItemBar
+      , "Foobar" .= cItemFoobar
+      ]
+
+-- a little error handling when processing the JSON input data
+processJsonData :: Either a b -> b
+processJsonData (Left _) = error "unable to parse data"
+processJsonData (Right x) = x
+
+-- IMPLEMENT
+generateCsvItems :: JsonItems -> Vector CsvItem
+generateCsvItems itemJson = Vector.fromList []
+
+encodeCsvItems :: Vector CsvItem -> ByteString
+encodeCsvItems = encodeDefaultOrderedByName . Foldable.toList
+
+encodeCsvItemsToFile :: FilePath -> Vector CsvItem -> IO ()
+encodeCsvItemsToFile filePath =
+  BS.writeFile filePath . encodeCsvItems
+
+-- read, process, and print out info from the data.json
 someFunc :: IO ()
 someFunc = do
-  d <- B.readFile "data.json"
+  f <- BS.readFile "data.json"
+  print f
+  let d = processJsonData (eitherDecode f :: Either String JsonItems)
   print d
+  let csvItems = generateCsvItems d
+  encodeCsvItemsToFile "items.csv" csvItems
+--let jsonData = decode f :: Maybe JsonList
+--putStrLn (encode jsonData)
+--let j = eitherDecode f
+--show $ liftIO $ Just j
+  --let j1 = JsonItem $ head j
+  --show j1
